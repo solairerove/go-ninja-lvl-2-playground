eeed1cd40fdf0582a78d0da70c3ba902a9681e66
Mikita Kryvitski <solairerove@gmail.com>
2019-07-05 23:41:21 +0300

    feature: complete ex1

--- lvl2exercise1/ex_1.go
+++ lvl2exercise1/ex_1.go
@@ -4,5 +4,10 @@ import "fmt"
 
 // Ex1 is a complete solution on jedi lvl 2
 func Ex1() {
-	fmt.Println("Ex1")
+
+	x := 42
+
+	fmt.Printf("%d\n", x)
+	fmt.Printf("%b\n", x)
+	fmt.Printf("%#x\n", x)
 }
