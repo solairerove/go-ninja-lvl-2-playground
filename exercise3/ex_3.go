d0f501844f7362ab2f2e8711a09d6bfae40126cf
Mikita Kryvitski <solairerove@gmail.com>
2019-07-05 23:54:51 +0300

    feature: complete ex 3 of lvl 2

---
+++ exercise3/ex_3.go
@@ -0,0 +1,15 @@
+package exercise3
+
+import "fmt"
+
+const (
+	x         = 3
+	y float64 = 64.42
+)
+
+// Ex3 is a complete solution on jedi lvl 2
+func Ex3() {
+
+	fmt.Println(x)
+	fmt.Println(y)
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	ex1 "github.com/solairerove/go-ninja-lvl-2-playground/exercise1"
 	ex2 "github.com/solairerove/go-ninja-lvl-2-playground/exercise2"
+	ex3 "github.com/solairerove/go-ninja-lvl-2-playground/exercise3"
 )
 
 func main() {
@@ -15,4 +16,9 @@ func main() {
 
 	fmt.Println("ex2()")
 	ex2.Ex2()
+
+	fmt.Println()
+
+	fmt.Println("ex3()")
+	ex3.Ex3()
 }
