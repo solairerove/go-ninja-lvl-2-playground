5f0205f812042518ec726eb63eab76c71f3bd0cf
Mikita Kryvitski <solairerove@gmail.com>
2019-07-05 23:36:48 +0300

    feature: init jedi lvl 2

---
+++ lvl2exercise1/ex_1.go
@@ -0,0 +1,8 @@
+package lvl2exercise1
+
+import "fmt"
+
+// Ex1 is a complete solution on jedi lvl 2
+func Ex1() {
+	fmt.Println("Ex1")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	ex1 "./lvl2exercise1"
+)
+
+func main() {
+	fmt.Println("ex1()")
+	ex1.Ex1()
+}
